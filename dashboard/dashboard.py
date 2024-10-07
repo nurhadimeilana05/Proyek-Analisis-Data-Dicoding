1d29aec8338db3aba83b59bea151c6e44a0b2e28
nurhadimeilana05 <126644029+nurhadimeilana05@users.noreply.github.com>
2024-10-08 06:51:59 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -61,14 +61,108 @@ main_df = all_df[
 function = AnalyticsTool(main_df)
 map_plot = BrazilGeospatial(data, plt, mpimg, urllib, st)
 
+sum_order_items_df = function.create_sum_order_items_df()
+recent_months_performance = function.create_monthly_performance_df()
 daily_order_df = function.create_daily_order_df()
 sum_spend_df = function.create_sum_spend_df()
-sum_order_items_df = function.create_sum_order_items_df()
-city, most_city = function.create_city_df()
+bycity_df, most_city = function.create_city_df()
 rfm_df = function.create_rfm_df()
 
 st.title("E-Commerce Public Dataset")
 st.write("**Hello Everyone! Welcome to Dashboard E-Commerce Public Dataset.**")
+
+# Order Items
+st.subheader("Order Items")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["order_item_count"].sum()
+    st.markdown(f"Total Items: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["order_item_count"].mean()
+    st.markdown(f"Average Items: **{avg_items:.2f}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(50, 25))
+
+colors = ["#72BF78", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+# Best Performing Product
+sns.barplot(
+    x="order_item_count", 
+    y="product_category_name_english", 
+    data=sum_order_items_df.head(5), 
+    palette=colors, 
+    ax=ax[0]
+)
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales (Order Item)", fontsize=80)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=90)
+ax[0].tick_params(axis='y', labelsize=55)
+ax[0].tick_params(axis='x', labelsize=50)
+
+# Worst Performing Product
+sns.barplot(
+    x="order_item_count", 
+    y="product_category_name_english", 
+    data=sum_order_items_df.sort_values(by="order_item_count", ascending=True).head(5), 
+    palette=colors, 
+    ax=ax[1]
+)
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales (Order Item)", fontsize=80)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=90)
+ax[1].tick_params(axis='y', labelsize=55)
+ax[1].tick_params(axis='x', labelsize=50)
+
+st.pyplot(fig)
+
+# Number of Order in Recent Month
+st.subheader("Number of Orders in Recent Months")
+
+fig, ax = plt.subplots(figsize=(10, 5))
+sns.lineplot(
+    x="order_purchase_timestamp",
+    y="order_count",
+    data=recent_months_performance,
+    marker="o",
+    linewidth=2,
+    label='Number of Orders'
+    color="#72BF78",
+    ax=ax
+)
+
+ax.set_title("Number of Orders in Recent Months (2018)", loc="center", fontsize=20)
+ax.set_ylabel("Number of Orders", fontsize=12)
+ax.set_xlabel("Month", fontsize=12)
+ax.tick_params(axis='x', labelsize=10)
+ax.tick_params(axis='y', labelsize=10)
+st.pyplot(fig)
+
+# Revenue in Recent Months
+st.subheader("Revenue in Recent Months")
+
+fig, ax = plt.subplots(figsize=(10, 5))
+sns.lineplot(
+    x="order_purchase_timestamp",
+    y="total_revenue",
+    data=recent_months_performance,
+    marker="o",
+    linewidth=2,
+    label='Revenue'
+    color="#72BF78",
+    ax=ax
+)
+
+ax.set_title("Revenue in Recent Months (2018)", loc="center", fontsize=20)
+ax.set_ylabel("Revenue", fontsize=12)
+ax.set_xlabel("Month", fontsize=12)
+ax.tick_params(axis='x', labelsize=10)
+ax.tick_params(axis='y', labelsize=10)
+st.pyplot(fig)
 
 # Daily Orders Delivered
 st.subheader("Daily Orders Delivered")
@@ -122,70 +216,21 @@ ax.tick_params(axis="x", rotation=45)
 ax.tick_params(axis="y", labelsize=15)
 st.pyplot(fig)
 
-# Order Items
-st.subheader("Order Items")
-col1, col2 = st.columns(2)
-
-with col1:
-    total_items = sum_order_items_df["order_item_count"].sum()
-    st.markdown(f"Total Items: **{total_items}**")
-
-with col2:
-    avg_items = sum_order_items_df["order_item_count"].mean()
-    st.markdown(f"Average Items: **{avg_items:.2f}**")
-
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(50, 25))
-
-colors = ["#72BF78", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
-
-# Best Performing Product
-sns.barplot(
-    x="order_item_count", 
-    y="product_category_name_english", 
-    data=sum_order_items_df.head(5), 
-    palette=colors, 
-    ax=ax[0]
-)
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales (Order Item)", fontsize=80)
-ax[0].set_title("Best Performing Product", loc="center", fontsize=90)
-ax[0].tick_params(axis='y', labelsize=55)
-ax[0].tick_params(axis='x', labelsize=50)
-
-# Worst Performing Product
-sns.barplot(
-    x="order_item_count", 
-    y="product_category_name_english", 
-    data=sum_order_items_df.sort_values(by="order_item_count", ascending=True).head(5), 
-    palette=colors, 
-    ax=ax[1]
-)
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales (Order Item)", fontsize=80)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Worst Performing Product", loc="center", fontsize=90)
-ax[1].tick_params(axis='y', labelsize=55)
-ax[1].tick_params(axis='x', labelsize=50)
-
-st.pyplot(fig)
-
 # Customer By City
 st.subheader("Customer By City")
 tab1, tab2 = st.tabs(["City", "Map"])
 
 with tab1:
-    most_city = city.customer_city.value_counts().idxmax()
-    st.markdown(f"Most Common City: **{most_city}**")
+    most_city_df = most_city
+    st.markdown(f"Most Common City: **{most_city_df}**")
     
     fig, ax = plt.subplots(figsize=(10, 5))
     colors = ["#72BF78"] + ["#D3D3D3"] * 4  
-
-    top_cities = city.customer_city.value_counts().head(5)
+    
     sns.barplot(
-        x=top_cities.index,
-        y=top_cities.values, 
+        x="customer_city",
+        y="customer_count",
+        data=bycity_df.head(5),
         palette=colors,
         ax=ax
     )
@@ -216,7 +261,7 @@ sns.barplot(
     ax=ax[0]
 )
 ax[0].set_ylabel("Recency (days)", fontsize=15)
-ax[0].set_xlabel("Customer ID", fontsize=30)
+ax[0].set_xlabel("Customer Unique ID", fontsize=30)
 ax[0].set_title("By Recency (days)", fontsize=50)
 ax[0].tick_params(axis='y', labelsize=15)
 ax[0].tick_params(axis='x', labelsize=15, rotation=90)
@@ -230,7 +275,7 @@ sns.barplot(
     ax=ax[1]
 )
 ax[1].set_ylabel("Frequency", fontsize=15)
-ax[1].set_xlabel("Customer ID", fontsize=30)
+ax[1].set_xlabel("Customer Unique ID", fontsize=30)
 ax[1].set_title("By Frequency", fontsize=50)
 ax[1].tick_params(axis='y', labelsize=15)
 ax[1].tick_params(axis='x', labelsize=15, rotation=90)
@@ -244,7 +289,7 @@ sns.barplot(
     ax=ax[2]
 )
 ax[2].set_ylabel("Monetary Value", fontsize=15)
-ax[2].set_xlabel("Customer ID", fontsize=30)
+ax[2].set_xlabel("Customer Unique ID", fontsize=30)
 ax[2].set_title("By Monetary", fontsize=50)
 ax[2].tick_params(axis='y', labelsize=15)
 ax[2].tick_params(axis='x', labelsize=15, rotation=90)
